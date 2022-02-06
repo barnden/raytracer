a5ccab24c1cfb381b6c5e9c0a96f4e0fb2270847
barnden <brandon@nguyen.vc>
2022-02-06 12:04:38 -0600

    Add is_same_cv<T, U> to check if typedef of T and U are the same regardless of cv-quals or lvalue/rvalue/pointer

--- src/util/Concepts.h
+++ src/util/Concepts.h
@@ -1,8 +1,13 @@
 #pragma once
 
 #include <cassert>
+#include <type_traits>
 
 #define ASSERT_NOT_REACHED assert(false);
 
 template <typename T>
 concept Numeric = std::convertible_to<T, std::size_t>;
+
+template <typename T, typename U>
+struct is_same_cv : std::is_same<typename std::decay<T>::type, typename std::decay<U>::type>::type {
+};
