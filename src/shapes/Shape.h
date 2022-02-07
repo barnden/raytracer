abf3cd93127e21dbcc1b1406e210e28b308cd5cc
barnden <brandon@nguyen.vc>
2022-02-07 00:14:08 -0600

    Shape: Fix ambiguous types; add getter for members

--- src/shapes/Shape.h
+++ src/shapes/Shape.h
@@ -9,13 +9,18 @@ private:
     Material m_material;
 
 public:
-    Shape(auto material)
+    Shape(Material material)
         : m_bounding_box(BoundingBox {})
         , m_material(material) {};
 
-    Shape(auto bounding_box, auto material)
+    Shape(BoundingBox bounding_box, Material material)
         : m_bounding_box(bounding_box)
         , m_material(material) {};
+
+    auto const& GetBoundingBox() const { return m_bounding_box; }
+    auto const& GetMaterial() const { return m_material; }
+
+    void SetBoundingBox(BoundingBox&& bounding_box) { m_bounding_box = std::move(bounding_box); }
 
     virtual Vec3<double> GetNormal(Vec3<double> point) const = 0;
     virtual double Intersect(Ray ray, double min, double max) const = 0;
